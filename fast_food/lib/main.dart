e208aff4b2f5264d03170ed68d8dd5d3e90529e7
juma6ek0v <sultanatleti@gmail.com>
2026-03-13 21:41:06 +0600

    init project

---
+++ fast_food/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(FastFoodApp());
+}
+
+class FastFoodApp extends StatelessWidget {
+  const FastFoodApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
